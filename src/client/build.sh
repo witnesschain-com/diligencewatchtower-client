265ab55a57609f950fa36f0f8619988842969404
Aswin <43544673+Aswin-ElMagnifico@users.noreply.github.com>
2024-06-10 15:26:27 +0530

    Add build script

---
+++ src/client/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+v=$(go version | { read _ _ v _; echo ${v#go}; })
+echo "$v"
+if [ "$v" != "1.20" ]; then
+    echo "go version not compatible, try experimental build.. exiting"
+    exit 1
+fi
+
+go build -o watchtower -ldflags '-X main.VERSION=v1.0.1-internal'
